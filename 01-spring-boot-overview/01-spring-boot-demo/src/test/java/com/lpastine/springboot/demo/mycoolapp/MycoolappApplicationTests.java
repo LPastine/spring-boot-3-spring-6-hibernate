1187f158a6d6b85376b8ca4ae006a708704bc186
LPastine <lucianopastine.tech@gmail.com>
2024-06-24 18:22:43 -0300

    spring boot initializer demo

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/test/java/com/lpastine/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpastine.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
