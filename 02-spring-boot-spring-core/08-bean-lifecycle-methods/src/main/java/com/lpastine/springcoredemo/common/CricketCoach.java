68a33652456ea48c72369f62adc05324425fc645
LPastine <lucianopastine.tech@gmail.com>
2024-06-28 08:24:07 -0300

    bean lifecycle methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/lpastine/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,30 @@
+package com.lpastine.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // 1) Define your methods for init and destroy
+    // 2) Add annotations: @PostConstruct and @PreDestroy
+    @PostConstruct
+    public void doMyStartUpStuff() {
+        System.out.println("In doMyStartUpStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("In doMyCleanUpStuff(): " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes!!";
+    }
+}
---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/lpastine/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.lpastine.springcoredemo.rest;
+
+import com.lpastine.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public void DemoController(
+            @Qualifier("cricketCoach") Coach theCoach
+    ) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
