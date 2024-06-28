99e81cac7eb781c2c56d99acd8ce2bc8cdfdfbf6
LPastine <lucianopastine.tech@gmail.com>
2024-06-28 06:26:06 -0300

    primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/lpastine/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,12 @@
+package com.lpastine.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/lpastine/springcoredemo/rest/DemoController.java
@@ -0,0 +1,22 @@
+package com.lpastine.springcoredemo.rest;
+
+import com.lpastine.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public void DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
