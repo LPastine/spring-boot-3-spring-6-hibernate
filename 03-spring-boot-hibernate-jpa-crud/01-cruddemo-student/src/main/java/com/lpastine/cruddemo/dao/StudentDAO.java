f4c713d95f749a90a5b05c78dc225456c48c4c8d
LPastine <lucianopastine.tech@gmail.com>
2024-07-03 06:24:42 -0300

    reading objects with jpa

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,10 @@ package com.lpastine.cruddemo.dao;
 
 import com.lpastine.cruddemo.entity.Student;
 
-// 1) Define DAO interface
 public interface StudentDAO {
 
     void save(Student theStudent);
+
+    // 1) Add new method to DAO interface
+    Student findById(Integer id);
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/dao/StudentDAOImpl.java
@@ -6,23 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-// 2) Define DAO implementation
 @Repository
 public class StudentDAOImpl implements StudentDAO {
 
-    // Define field for entity manager
     private EntityManager entityManager;
 
-    // Inject entity manager using constructor injection
     @Autowired
     public StudentDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    // Implement save method
     @Override
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    // 2) Add new method to DAO implementation
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
     }
 }
