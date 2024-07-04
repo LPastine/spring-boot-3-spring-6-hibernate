a065037e19b57b8bf7f2ecbdf4e81482d566952b
LPastine <lucianopastine.tech@gmail.com>
2024-07-04 06:45:44 -0300

    create database tables from java code

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
@@ -21,13 +21,13 @@ public class CruddemoApplication {
 
 		return runner -> {
 //			createStudent(studentDAO);
-//			createMultipleStudents(studentDAO);
+			createMultipleStudents(studentDAO);
 //			readStudent(studentDAO);
 //			queryForStudents(studentDAO);
 //			queryForStudentsByLastName(studentDAO);
 //			updateStudent(studentDAO);
 //			deleteStudent(studentDAO);
-			deleteAllStudents(studentDAO);
+//			deleteAllStudents(studentDAO);
 		};
 	}
 
