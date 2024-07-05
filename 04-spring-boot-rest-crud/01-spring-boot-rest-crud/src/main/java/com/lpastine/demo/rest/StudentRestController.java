bd48909c755633a63fac10daab0d5f2b298495c5
LPastine <lucianopastine.tech@gmail.com>
2024-07-05 06:02:57 -0300

    spring boot rest path variables

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/lpastine/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/lpastine/demo/rest/StudentRestController.java
@@ -1,28 +1,43 @@
 package com.lpastine.demo.rest;
 
 import com.lpastine.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// 2) Create Spring REST Service using @RestController
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // Define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    // Define @PostConstruct to load the student data ... only once
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+    // Define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    // Define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
