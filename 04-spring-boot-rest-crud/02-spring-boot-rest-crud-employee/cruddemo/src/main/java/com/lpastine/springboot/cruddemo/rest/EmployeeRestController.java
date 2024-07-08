f03ec9717cd89deb406a53b45bd2d27f52d77345
LPastine <lucianopastine.tech@gmail.com>
2024-07-08 06:53:27 -0300

    spring boot rest, update employee

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/lpastine/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/lpastine/springboot/cruddemo/rest/EmployeeRestController.java
@@ -47,4 +47,13 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
 }
