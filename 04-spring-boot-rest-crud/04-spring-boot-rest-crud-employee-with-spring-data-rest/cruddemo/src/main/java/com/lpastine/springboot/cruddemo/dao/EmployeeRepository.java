98b8bd6cef19b47ffcf38af248f80738ff87fe1c
LPastine <lucianopastine.tech@gmail.com>
2024-07-08 08:13:28 -0300

    spring data rest configs

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/cruddemo/src/main/java/com/lpastine/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/cruddemo/src/main/java/com/lpastine/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.lpastine.springboot.cruddemo.dao;
 
 import com.lpastine.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// In case we don't want to customize the automatically generated path
+// based on the class's name, we can specify the path
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it... no need to write any code
