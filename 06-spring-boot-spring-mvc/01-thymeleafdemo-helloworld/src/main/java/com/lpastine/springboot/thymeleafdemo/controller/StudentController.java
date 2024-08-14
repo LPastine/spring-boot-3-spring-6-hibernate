24d7a29f8f68213d10a54e1c8e730b4c94aaa19f
LPastine <lucianopastine.tech@gmail.com>
2024-08-14 08:10:06 -0300

    spring mvc form data binding check boxes

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/controller/StudentController.java
@@ -15,6 +15,9 @@ public class StudentController {
     @Value("${countries}")
     private List<String> countries;
 
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -26,6 +29,9 @@ public class StudentController {
 
         // add the list of countries to the model
         theModel.addAttribute("countries", countries);
+
+        // add the list of languages to the model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
