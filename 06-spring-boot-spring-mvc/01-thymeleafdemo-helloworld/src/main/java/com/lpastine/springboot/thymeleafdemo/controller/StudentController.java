f6bd03abd373f9942512dd0a4d9e21c03b27613b
LPastine <lucianopastine.tech@gmail.com>
2024-08-14 08:37:45 -0300

    spring mvc form data binding check boxes

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/controller/StudentController.java
@@ -18,6 +18,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
@@ -32,6 +35,9 @@ public class StudentController {
 
         // add the list of languages to the model
         theModel.addAttribute("languages", languages);
+
+        // add the list of languages to the model
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }
--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/lpastine/springboot/thymeleafdemo/model/Student.java
@@ -1,10 +1,13 @@
 package com.lpastine.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
     public Student() {}
 
@@ -38,5 +41,13 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
