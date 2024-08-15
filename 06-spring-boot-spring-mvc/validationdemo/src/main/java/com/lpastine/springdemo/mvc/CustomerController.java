c08d787d78438dbc0856b40eebd893de92e08f6c
LPastine <lucianopastine.tech@gmail.com>
2024-08-15 07:50:49 -0300

    spring mvc validation, debugging tips for custom error names

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/lpastine/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/lpastine/springdemo/mvc/CustomerController.java
@@ -36,6 +36,12 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
+        System.out.println("Binding results: " + theBindingResult.toString());
+
+        System.out.println("\n\n\n\n");
+
         if (theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
