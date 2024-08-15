d4960d50483824047c85b5e9ef9d426f8ee768f6
LPastine <lucianopastine.tech@gmail.com>
2024-08-15 08:55:41 -0300

    spring mvc validation, custom validation

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/lpastine/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/lpastine/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.lpastine.springdemo.mvc;
 
+import com.lpastine.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -17,6 +18,9 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value="LP", message="must start with LP")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -48,5 +52,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
