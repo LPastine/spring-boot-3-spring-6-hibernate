42700bfef099a242689c6d3df27a91b19e92a187
LPastine <lucianopastine.tech@gmail.com>
2024-08-20 06:36:19 -0300

    crud db project, delete employee

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -56,7 +56,6 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
-
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 
@@ -65,6 +64,15 @@ public class EmployeeController {
 
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
 
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to the /employees/list
+        return "redirect:/employees/list";
     }
 }
