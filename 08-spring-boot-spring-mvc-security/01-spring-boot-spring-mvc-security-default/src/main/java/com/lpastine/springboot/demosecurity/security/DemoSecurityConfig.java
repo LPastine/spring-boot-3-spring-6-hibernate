261df1803f036634a60c5fb8fd9c92b5776e1ca0
LPastine <lucianopastine.tech@gmail.com>
2024-08-21 06:27:29 -0300

    spring mvc security, logout

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/lpastine/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/lpastine/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -48,6 +48,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
                 );
 
         return http.build();
