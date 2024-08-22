a7a046aefcc6473f21e720cb48c2f9f1f55fa2ac
LPastine <lucianopastine.tech@gmail.com>
2024-08-22 06:41:41 -0300

    spring mvc security, jdbc authentication custom tables

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/lpastine/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/lpastine/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -16,7 +16,19 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager (DataSource dataSource) {
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // Define a query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        // Define a query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
