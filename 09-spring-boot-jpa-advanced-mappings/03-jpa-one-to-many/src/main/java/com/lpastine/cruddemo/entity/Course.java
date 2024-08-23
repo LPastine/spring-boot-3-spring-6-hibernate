db215caae04bdc3df560ffc949b964d74cd86e75
LPastine <lucianopastine.tech@gmail.com>
2024-08-23 07:34:26 -0300

    annotate course entity

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/entity/Course.java
@@ -1,8 +1,6 @@
 package com.lpastine.cruddemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "course")
@@ -10,10 +8,20 @@ public class Course {
 
     // define and annotate our fields
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     private String title;
 
+    @ManyToOne(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH,
+            CascadeType.REFRESH,
+    })
+    @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
     // define constructors
