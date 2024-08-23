e2a03d61ac2a73782cec013dd9d9b919278775e0
LPastine <lucianopastine.tech@gmail.com>
2024-08-23 08:01:27 -0300

    annotate instructor entity

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/entity/Instructor.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.lpastine.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "instructor")
 public class Instructor {
@@ -37,6 +40,16 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+            cascade = {
+                        CascadeType.PERSIST,
+                        CascadeType.MERGE,
+                        CascadeType.DETACH,
+                        CascadeType.REFRESH,
+            }
+    )
+    private List<Course> courses;
 
     public Instructor() {}
 
@@ -86,6 +99,14 @@ public class Instructor {
         this.instructorDetail = instructorDetail;
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     @Override
     public String toString() {
         return "Instructor{" +
@@ -95,5 +116,17 @@ public class Instructor {
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
                 '}';
+    }
+
+    // add convenience methods for bi-directional relationship
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
     }
 }
