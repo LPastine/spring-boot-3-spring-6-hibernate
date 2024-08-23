b0930fb2d39ec93ce69d8fe8edb5cd2b3ce5e7a4
LPastine <lucianopastine.tech@gmail.com>
2024-08-23 17:46:02 -0300

    one to many, join fetch courses

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/lpastine/cruddemo/dao/AppDAOImpl.java
@@ -82,4 +82,23 @@ public class AppDAOImpl implements AppDAO {
 
         return courses;
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        // create query
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i "
+                + "JOIN FETCH i.courses "
+                + "JOIN FETCH i.instructorDetail "
+                + "where i.id = :data",
+                Instructor.class
+        );
+
+        query.setParameter("data", theId);
+
+        // execute query
+        Instructor instructor = query.getSingleResult();
+
+        return instructor;
+    }
 }
