72845a3f5135af7a6f3ef7a1f785f538523812e9
LPastine <lucianopastine.tech@gmail.com>
2024-08-26 06:55:24 -0300

    one to many uni, refactor course entity

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/lpastine/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/lpastine/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package com.lpastine.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -23,6 +26,13 @@ public class Course {
     })
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     // define constructors
     public Course() {}
@@ -54,6 +64,23 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+    public void addReview(Review theReview) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
     }
 
     // define toString
