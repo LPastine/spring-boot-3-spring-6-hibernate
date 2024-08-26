c237153e9e22067a3d243a21e06c7af92b4e1801
LPastine <lucianopastine.tech@gmail.com>
2024-08-26 08:44:22 -0300

    many to many - create course and students

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
@@ -1,10 +1,7 @@
 package com.lpastine.cruddemo;
 
 import com.lpastine.cruddemo.dao.AppDAO;
-import com.lpastine.cruddemo.entity.Course;
-import com.lpastine.cruddemo.entity.Instructor;
-import com.lpastine.cruddemo.entity.InstructorDetail;
-import com.lpastine.cruddemo.entity.Review;
+import com.lpastine.cruddemo.entity.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,7 +21,39 @@ public class CruddemoApplication {
 
 		return runner -> {
 
+			createCourseAndStudents(appDAO);
+
 		};
+	}
+
+	private void createCourseAndStudents(AppDAO appDAO) {
+
+		// create a course
+		Course tempCourse = new Course("Pacman - How To Score One Million Points");
+
+		// create the students
+		Student tempStudent1 = new Student(
+				"John",
+				"Doe",
+				"john@luv2code.com"
+		);
+		Student tempStudent2 = new Student(
+				"Mary",
+				"Public",
+				"mary@luv2code.com"
+		);
+
+		// add students to the course
+		tempCourse.addStudent(tempStudent1);
+		tempCourse.addStudent(tempStudent2);
+
+		// save the course and associated students
+		System.out.println("Saving the course: " + tempCourse);
+		System.out.println("associated students: " + tempCourse.getStudents());
+
+		appDAO.save(tempCourse);
+
+		System.out.println("Done!");
 	}
 
 	private void deleteCourseAndReviews(AppDAO appDAO) {
