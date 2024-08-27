9a81185c9b289960e84aeae3675839e883a8e4f8
LPastine <lucianopastine.tech@gmail.com>
2024-08-27 05:44:23 -0300

    delete course

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/lpastine/cruddemo/CruddemoApplication.java
@@ -27,7 +27,9 @@ public class CruddemoApplication {
 
 			// findStudentAndCourses(appDAO);
 
-			addMoreCoursesForStudent(appDAO);
+			// addMoreCoursesForStudent(appDAO);
+
+			deleteCourse(appDAO);
 
 		};
 	}
